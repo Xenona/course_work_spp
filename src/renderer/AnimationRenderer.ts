29dc737c27d12c29919b9660fabdcfd48f05469c
Ksenia Trubach <k.trubach@yadro.com>
2025-11-12 01:51:02 +0300

    Sync times

--- src/renderer/AnimationRenderer.ts
+++ src/renderer/AnimationRenderer.ts
@@ -7,7 +7,7 @@ export class AnimationRenderer extends ObjectRenderer<BoardAnimation> {
   render(r: Renderer, ctx: CanvasRenderingContext2D) {
     if (this.target.objects.length == 0) return
 
-    const time = performance.now() / 500
+    const time = (Date.now()) / 500
     const id =
       this.target.objects[Math.floor(time) % this.target.objects.length]
     r.getObjectRenderer(id)!.render(r, ctx)
